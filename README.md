4fcc2542bbef75471784ed4ca07eb19a480fc28b
Tejas Sharma <tejas.s1996@gmail.com>
2018-04-14 13:29:32 -0500

    updated readme and sentiment analysis works

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 410-Final-Project
+Music Rater
+===========
+
+Our CS410 Final Project. It uses sentiment analysis and pitchfork reviews to assign an artist a rating, and allows users to rate artists in order to form a content based recommendation system. This project uses the VADER sentiment analysis library available in nltk.
+
+Installation
+------------
+Clone the repo and install the packages in requirements.txt
+
+After installing, make sure to download the necessary nltk files with the following commands in python:
+
+```python
+>> nltk.download('punkt')
+>> nltk.download('vader_lexicon')
+```
--- artist_rating.py
+++ artist_rating.py
@@ -1,5 +1,7 @@
 """This module will be used to assign an artist a score based on their albums."""
 import pitchfork
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
 
 
 def get_pitchfork_reviews(artist_name, album_names):
@@ -17,6 +19,28 @@ def get_pitchfork_reviews(artist_name, album_names):
             # This is necessary because sometimes albums with similar names
             # get added twice as the pitchfork search is fuzzy
             if review not in temp:
-                temp.append(reviews)
+                temp.append(review)
         reviews = temp
     return reviews
+
+
+def get_sentiment_rating(review_text):
+    """Use sentiment analysis to rate a review as positive or negative."""
+    sentences = tokenize.sent_tokenize(review_text)
+    sid = SentimentIntensityAnalyzer()
+    ss = [sid.polarity_scores(sentence) for sentence in sentences]
+    avg_sentiment = {k: sum(t[k] for t in ss) / len(ss) for k in ss[0]}
+    # Compound score (low = more negative) is scaled from -1 to 1
+    # This scales it from 0 to 10 instead
+    compound = (avg_sentiment['compound'] + 1) * 5
+    return compound
+
+
+def get_overall_rating(reviews):
+    """Get the overall rating based on a set of reviews."""
+    for review in reviews:
+        review.sentiment = get_sentiment_rating(review.full_text())
+        review.overall_rating = 0.8 * review.score() + 0.2 * review.sentiment
+    total = sum(review.overall_rating for review in reviews)
+    avg = total / len(reviews)
+    return avg
