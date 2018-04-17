59e75538f9abadc657a45a21d5f64734afcc8ecd
Tejas Sharma <tejas.s1996@gmail.com>
2018-04-17 12:59:45 -0500

    slight cleanup and documentation edits

--- project.py
+++ project.py
@@ -1,67 +1,78 @@
+"""The flasks server that runs our project."""
 from flask import Flask, render_template, session, redirect, url_for, request, jsonify
-from api_keys import spotify_client_id, spotify_client_secret
 import psycopg2
 # import credentials
-import spotipy
 import album_discovery
 import artist_rating
-from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
-spotify_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id,
-                                                       client_secret=spotify_client_secret)
-sp = spotipy.Spotify(client_credentials_manager=spotify_credentials_manager)
+sp = album_discovery.sp
+
 
 @app.route('/')
 def index():
+    """Endpoint for the home page."""
     # test_db()
     return render_template('index.html')
 
+
 @app.route('/login', methods=['POST'])
 def login():
+    """Login method."""
     session['email'] = request.form['email']
     return redirect(url_for('user', name=session['email']))
 
+
 @app.route('/signup', methods=['POST'])
 def signup():
+    """Sign a user up."""
     user_name = request.form['name']
     return redirect(url_for('user', name=user_name))
 
+
 @app.route('/user/<name>')
 def user(name):
+    """Show the user some recommendations."""
     return render_template('user.html', name=name, recs=get_user_recommendations(1))
 
 
 @app.route('/user/<id>/recommendations')
 def get_user_recommendations(id):
+    """Get recommendations based on a user's id."""
     # print(artist_rating.get_rating_from_query('Drake'))
 
     # return json object of hard coded artist for now
     return jsonify([sp.artist('4xRYI6VqpkE3UwrDrAZL8L'), sp.artist('3TVXtAsR1Inumwj472S9r4'), sp.artist('26VFTg2z8YR0cCuwLzESi2'), sp.artist('1Bl6wpkWCQ4KVgnASpvzzA'), sp.artist('536BYVgOnRky0xjsPT96zl'), sp.artist('4kI8Ie27vjvonwaB2ePh8T')])
 
+
 @app.route('/new_artist', methods=['GET', 'POST'])
 def new_artist():
+    """Search page for adding a new artist to the db."""
     return render_template("new_artist.html")
+
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    """Autocomplete endpoint used for adding a new artist."""
     search = request.args.get('q')
     print(search)
     artist_list = album_discovery.get_artist_list(search)
     results = [artist['name'] for artist in artist_list]
     return jsonify(matching_results=results)
 
+
 @app.route('/handledata/', methods=['POST'])
 def handledata():
-    value = request.form['artist_name']
-    print(request.form['artist_name'])
-    return 'Some response'
+    """Used to handle an artist request."""
+    artist_name = request.form['artist_name']
+    artist_rate = artist_rating.get_rating_from_query(artist_name)
+    print(artist_name, artist_rate)
+    return jsonify(artist=artist_name, rating=artist_rate)
 
-"""
-This function simply serves to test that the postegres database is setup and working properly
-"""
+
 def test_db():
+    """Test that the postegres database is setup and working properly."""
     dbname = 'cs410project'
     user = credentials.login['user']
     password = credentials.login['password']
