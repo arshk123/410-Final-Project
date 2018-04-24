1a444f0a92bc7de7106009ea57fdf2b5e6df678e
Arsh Khandelwal <khandelwal.arsh@gmail.com>
2018-04-24 16:43:13 -0500

    merging

--- recommender.py
+++ recommender.py
@@ -3,9 +3,11 @@ from artist_rating import *
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from api_keys import spotify_client_id, spotify_client_secret
-from surprise import evaluate
+from surprise import evaluate, Dataset, Reader
 from surprise import KNNBasic
+from collections import defaultdict
 import random
+import pandas as pd
 
 sample_artists = [ 'Goldlink', 'Cage the elephant', 'DNCE', 'T-pain', 'Kings of Leon', 'Hurley Mower', 'Shallou', 'Khalid', 'Ramin Djawadi', 'Ed Sheeran', 'Kid Cudi', 'Vengaboys', 'Calvin Harris', 'The Weekend', 'Drake', 'Lil Dicky', 'Bowling for Soup', 'XXXTentacion', 'Post Malone', 'A$ap rocky', 'Illenium', 'Logic']
 scm = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
@@ -18,6 +20,7 @@ class Recommender:
         self.pg = pg
         self.testing = testing
 
+    """ Doesn't work """
     def top3(self, predictions, topN = 3):
         top_recs = defaultdict(list)
         for uid, iid, true_r, est, _ in predictions:
@@ -30,14 +33,19 @@ class Recommender:
         return top_recs
 
     def setup(self, data):
+        df = pd.DataFrame.from_dict(data)
+        reader = Reader(line_format='user item rating', rating_scale=(1, 10))
+        data = Dataset.load_from_df(df[['userID', 'itemID', 'ratings']], reader)
+        train = data.build_full_trainset()
         sim_options = {
             'name': 'cosine',
-            'user_based': False
+            'user_based': True
         }
         self.knn = KNNBasic(sim_options=sim_options)
-        self.knn.fit(data)
-        testSet = data.build_anti_testset()
-        predictions = knn.test(testSet)
+        self.knn.fit(train)
+        test = train.build_anti_testset()
+        predictions = self.knn.test(test)
+        print(predictions)
         topPreds = self.top3(predictions)
         print(topPreds)
 
@@ -45,26 +53,47 @@ class Recommender:
         pass
 
     def buildSampleDataset(self, numSamples=100):
-        ret = []
+        data = {
+            'userID' : [],
+            'itemID' : [],
+            'ratings' : []
+        }
+        users = []
+        ratings = []
+        items = []
         labels = []
         for artist in sample_artists:
             results = sp.search(q='artist:' + artist, type='artist')
-            items = results['artists']['items']
-            if(len(items) > 0):
-                for i, a in enumerate(items):
-                    print(i, a['name'])
-                self.labels.append(a['name'])
-            for a in range(numSamples):
-                for artist in self.labels:
-                    ret.append([artist, random.randint(1,11)])
-            # self.labels = labels
-            return ret, labels
+            r = results['artists']['items']
+            if(len(r) > 0):
+                for i, a in enumerate(r):
+                    # print(i, a['name'])
+                    self.labels.append(a['name'])
+
+        for ind, art in enumerate(self.labels):
+            for num in range(numSamples):
+                if random.randint(1,11)%2 != 0:
+                    users.append(num)
+                    items.append(ind)
+                    ratings.append(random.randint(1,11))
+        # print(len(users), len(ratings), len(items))
+        data['itemID'] = items
+        data['userID'] = users
+        data['ratings'] = ratings
+        # print(data)
+        return data, labels
 
     def recommend(self, artist):
         pass
 
+"""
+data = {
+    'itemID' : [1, 5, 4, 3, 2],
+    'userID' : [9, 32, 16, 4, 31],
+    'rating' : [1, 2, 3, 5, 8]
+}
+"""
 
 recommender = Recommender()
 data, labels = recommender.buildSampleDataset()
-print(data[0])
 recommender.setup(data)
