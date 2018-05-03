08efcc2021ab01eb27da5220afa035a491627a0c
Tejas Sharma <tejas.s1996@gmail.com>
2018-05-03 14:07:06 -0500

    :thinking:

--- static/navsearch.js
+++ static/navsearch.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#navsearch").autocomplete({
         source:function(request, response) {
             console.log(request.term)
-            $.getJSON("{{url_for('navsearch')}}",{
+            $.getJSON("/navsearch",{
                 q: request.term, // in flask, "q" will be the argument to look for using request.args
             }, function(data) {
                 response(data.matching_results); // matching_results from jsonify
