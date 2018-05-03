12b5f03ddc3be3cd170701f3dab590a12a598f3a
Tejas Sharma <tejas.s1996@gmail.com>
2018-05-03 14:00:49 -0500

    attempting to fix autocomplete is not a function error

---
+++ static/navsearch.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#navsearch").autocomplete({
+        source:function(request, response) {
+            console.log(request.term)
+            $.getJSON("{{url_for('navsearch')}}",{
+                q: request.term, // in flask, "q" will be the argument to look for using request.args
+            }, function(data) {
+                response(data.matching_results); // matching_results from jsonify
+            });
+        },
+        minLength: 3,
+        select: function(event, ui) {
+            window.location.replace("/artist/" + ui.item.value);
+        }
+    });
+});
