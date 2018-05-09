cccfc7066b5301c5d1d7ab341772c25e3a4f4b21
Tejas Sharma <tejas.s1996@gmail.com>
2018-05-09 17:27:49 -0500

    still trying

--- static/top_artists.js
+++ static/top_artists.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
     $.get('/top_artists_source', function(data, status){
         console.log(data)
-        $('.container').append('<div id="card_deck" class="card-deck"></div>');
+        $('.card-deck-wrapper').append('<div id="card_deck" class="card-deck"></div>');
         for (var i = 0; i < data.length; i++ ){
             if (data[i].images.length != 0){
                 var recommended_artist_html = `
-                <div class="col-4">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top img-fluid" src="${data[i].images[0].url}" />
-                        <div class="card-body">
-                            <h5 class="card-title">${data[i].name}</h5>
-                            <p class="card-text">Genre: ${data[i].genres[0]}</p>
-                            <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
-                        </div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" src="${data[i].images[0].url}" />
+                    <div class="card-body">
+                        <h5 class="card-title">${data[i].name}</h5>
+                        <p class="card-text">Genre: ${data[i].genres[0]}</p>
+                        <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
                     </div>
                 </div>
                 `;
@@ -20,14 +18,12 @@ $(document).ready(function(){
             }
             else{
                 var recommended_artist_html = `
-                <div class="col-4">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top img-fluid" src="/static/album-art-empty.png" />
-                        <div class="card-body">
-                            <h5 class="card-title">${data[i].name}</h5>
-                            <p class="card-text">Genre: ${data[i].genres[0]}</p>
-                            <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
-                        </div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" src="/static/album-art-empty.png" />
+                    <div class="card-body">
+                        <h5 class="card-title">${data[i].name}</h5>
+                        <p class="card-text">Genre: ${data[i].genres[0]}</p>
+                        <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
                     </div>
                 </div>
                 `;
