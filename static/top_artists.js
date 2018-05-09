5568594c17540ed0594369ad293d886f1f995aaa
Tejas Sharma <tejas.s1996@gmail.com>
2018-05-09 17:19:14 -0500

    givin up?

--- static/top_artists.js
+++ static/top_artists.js
@@ -5,16 +5,34 @@ $(document).ready(function(){
         for (var i = 0; i < data.length; i++ ){
             if (data[i].images.length != 0){
                 var recommended_artist_html = `
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top img-fluid" src="${data[i].images[0].url}" />
-                    <div class="card-body">
-                        <h5 class="card-title">${data[i].name}</h5>
-                        <p class="card-text">Genre: ${data[i].genres[0]}</p>
-                        <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top img-fluid" src="${data[i].images[0].url}" />
+                        <div class="card-body">
+                            <h5 class="card-title">${data[i].name}</h5>
+                            <p class="card-text">Genre: ${data[i].genres[0]}</p>
+                            <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
+                        </div>
                     </div>
                 </div>
                 `;
                 $('#card_deck').append(recommended_artist_html)
+            }
+            else{
+                var recommended_artist_html = `
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top img-fluid" src="/static/album-art-empty.png" />
+                        <div class="card-body">
+                            <h5 class="card-title">${data[i].name}</h5>
+                            <p class="card-text">Genre: ${data[i].genres[0]}</p>
+                            <a href="/artist/${data[i].id}" class="btn btn-primary">View Artist Page</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+                $('#row' + row_number).append(recommended_artist_html)
+                // $('#row' + i).append('<a href="#" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-felx w-100 justify-content-between"><h5 class="mb-1">'+data[i].name+'</h5></div></a>')
             }
         }
     });
